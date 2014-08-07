0a1ce63ef6fee78c5db2f482ac0a11df6179657a
beergeek <brett.gray@puppetlabs.com>
2014-08-07 14:53:52 -0700

    test

--- manifests/c_0003.pp
+++ manifests/c_0003.pp
@@ -1,69 +1,69 @@
 class shit_logic::c_0003 {
 
-  file { '/etc/crtestdir/c0003r10_file0001_of_10':
+  file { '/etc/c0003r10_file0001_of_10':
     ensure => present,
     mode => '0666',
     content => 'CONTENT c0003r0001\n',
   }
 
 
-  file { '/etc/crtestdir/c0003r10_file0002_of_10':
+  file { '/etc/c0003r10_file0002_of_10':
     ensure => present,
     mode => '0666',
     content => 'CONTENT c0003r0002\n',
   }
 
 
-  file { '/etc/crtestdir/c0003r10_file0003_of_10':
+  file { '/etc/c0003r10_file0003_of_10':
     ensure => present,
     mode => '0666',
     content => 'CONTENT c0003r0003\n',
   }
 
 
-  file { '/etc/crtestdir/c0003r10_file0004_of_10':
+  file { '/etc/c0003r10_file0004_of_10':
     ensure => present,
     mode => '0666',
     content => 'CONTENT c0003r0004\n',
   }
 
 
-  file { '/etc/crtestdir/c0003r10_file0005_of_10':
+  file { '/etc/c0003r10_file0005_of_10':
     ensure => present,
     mode => '0666',
     content => 'CONTENT c0003r0005\n',
   }
 
 
-  file { '/etc/crtestdir/c0003r10_file0006_of_10':
+  file { '/etc/c0003r10_file0006_of_10':
     ensure => present,
     mode => '0666',
     content => 'CONTENT c0003r0006\n',
   }
 
 
-  file { '/etc/crtestdir/c0003r10_file0007_of_10':
+  file { '/etc/c0003r10_file0007_of_10':
     ensure => present,
     mode => '0666',
     content => 'CONTENT c0003r0007\n',
   }
 
 
-  file { '/etc/crtestdir/c0003r10_file0008_of_10':
+  file { '/etc/c0003r10_file0008_of_10':
     ensure => present,
     mode => '0666',
     content => 'CONTENT c0003r0008\n',
   }
 
 
-  file { '/etc/crtestdir/c0003r10_file0009_of_10':
+  file { '/etc/c0003r10_file0009_of_10':
     ensure => present,
     mode => '0666',
     content => 'CONTENT c0003r0009\n',
   }
 
 
-  file { '/etc/crtestdir/c0003r10_file0010_of_10':
+  file { '/etc/c0003r10_file0010_of_10':
     ensure => present,
     mode => '0666',
     content => 'CONTENT c0003r0010\n',
--- manifests/c_0010.pp
+++ manifests/c_0010.pp
@@ -1,69 +1,69 @@
 class shit_logic::c_0010 {
 
-  file { '/etc/crtestdir/c0010r10_file0001_of_10':
+  file { '/etc/c0010r10_file0001_of_10':
     ensure => present,
     mode => '0666',
     content => 'CONTENT c0010r0001\n',
   }
 
 
-  file { '/etc/crtestdir/c0010r10_file0002_of_10':
+  file { '/etc/c0010r10_file0002_of_10':
     ensure => present,
     mode => '0666',
     content => 'CONTENT c0010r0002\n',
   }
 
 
-  file { '/etc/crtestdir/c0010r10_file0003_of_10':
+  file { '/etc/c0010r10_file0003_of_10':
     ensure => present,
     mode => '0666',
     content => 'CONTENT c0010r0003\n',
   }
 
 
-  file { '/etc/crtestdir/c0010r10_file0004_of_10':
+  file { '/etc/c0010r10_file0004_of_10':
     ensure => present,
     mode => '0666',
     content => 'CONTENT c0010r0004\n',
   }
 
 
-  file { '/etc/crtestdir/c0010r10_file0005_of_10':
+  file { '/etc/c0010r10_file0005_of_10':
     ensure => present,
     mode => '0666',
     content => 'CONTENT c0010r0005\n',
   }
 
 
-  file { '/etc/crtestdir/c0010r10_file0006_of_10':
+  file { '/etc/c0010r10_file0006_of_10':
     ensure => present,
     mode => '0666',
     content => 'CONTENT c0010r0006\n',
   }
 
 
-  file { '/etc/crtestdir/c0010r10_file0007_of_10':
+  file { '/etc/c0010r10_file0007_of_10':
     ensure => present,
     mode => '0666',
     content => 'CONTENT c0010r0007\n',
   }
 
 
-  file { '/etc/crtestdir/c0010r10_file0008_of_10':
+  file { '/etc/c0010r10_file0008_of_10':
     ensure => present,
     mode => '0666',
     content => 'CONTENT c0010r0008\n',
   }
 
 
-  file { '/etc/crtestdir/c0010r10_file0009_of_10':
+  file { '/etc/c0010r10_file0009_of_10':
     ensure => present,
     mode => '0666',
     content => 'CONTENT c0010r0009\n',
   }
 
 
-  file { '/etc/crtestdir/c0010r10_file0010_of_10':
+  file { '/etc/c0010r10_file0010_of_10':
     ensure => present,
     mode => '0666',
     content => 'CONTENT c0010r0010\n',
